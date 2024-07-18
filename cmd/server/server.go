d6fa4dd2f12217a04cc9b81f9ec069ea1520e849
Maksim Litvinov <metalim@gmail.com>
2024-07-18 13:11:31 +0300

    serve /files/*

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -13,6 +13,14 @@ import (
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
+	var dir string
+	if len(os.Args) > 2 && os.Args[1] == "--directory" {
+		dir = os.Args[2]
+		if !strings.HasSuffix(dir, "/") {
+			dir += "/"
+		}
+	}
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -25,11 +33,11 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handleConnection(con)
+		go handleConnection(con, dir)
 	}
 }
 
-func handleConnection(con net.Conn) {
+func handleConnection(con net.Conn, dir string) {
 	defer con.Close()
 	con.SetDeadline(time.Now().Add(5 * time.Second))
 
@@ -40,7 +48,7 @@ func handleConnection(con net.Conn) {
 		return
 	}
 
-	var res string
+	res := "HTTP/1.1 404 Not Found\r\n\r\n"
 	switch {
 	case request.Method == "GET" && request.URL.Path == "/":
 		res = "HTTP/1.1 200 OK\r\n\r\n"
@@ -51,8 +59,23 @@ func handleConnection(con net.Conn) {
 	case request.Method == "GET" && request.URL.Path == "/user-agent":
 		res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(request.UserAgent()), request.UserAgent())
 
-	default:
-		res = "HTTP/1.1 404 Not Found\r\n\r\n"
+	case request.Method == "GET" && strings.HasPrefix(request.URL.Path, "/files/"):
+		file, err := os.Open(dir + request.URL.Path[7:])
+		if err != nil {
+			break
+		}
+		defer file.Close()
+
+		fi, err := file.Stat()
+		if err != nil {
+			break
+		}
+		size := fi.Size()
+		res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n", size)
+		con.Write([]byte(res))
+		io := bufio.NewReader(file)
+		io.WriteTo(con)
+		return
 	}
 	con.Write([]byte(res))
 }
